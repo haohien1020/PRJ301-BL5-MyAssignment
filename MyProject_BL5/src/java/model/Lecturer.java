1cec8990445a5d16097eee6200bd4f5760c6d7d8
haohien1020 <108945232+haohien1020@users.noreply.github.com>
2023-04-25 06:32:51 +0700

    tạo classs lecturer

---
+++ MyProject_BL5/src/java/model/Lecturer.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author HP
+ */
+public class Lecturer {
+    private int lid ;
+    private String lname ;
+
+    public Lecturer(int lid, String lname) {
+        this.lid = lid;
+        this.lname = lname;
+    }
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public Lecturer() {
+    }
+    
+}
